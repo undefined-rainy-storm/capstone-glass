e55d32484b4dd37d8a8a6bf64a692caeec0c95ff
Jonghyeon Park <belline0124@gmail.com>
2024-11-29 01:16:36 +0900

    chore: fix indent

--- controller/controller.ino
+++ controller/controller.ino
@@ -5,25 +5,25 @@
 #include <BLE2902.h>
 
 // Camera pins for AI Thinker ESP32-CAM
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 // BLE settings
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 BLEServer* pServer = NULL;
@@ -31,105 +31,102 @@ BLECharacteristic* pCharacteristic = NULL;
 bool deviceConnected = false;
 const int MTU_SIZE = 512;  // Maximum transmission unit size
 
-class ServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-        deviceConnected = true;
-    }
-    void onDisconnect(BLEServer* pServer) {
-        deviceConnected = false;
-    }
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  }
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
 };
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // Configure camera
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_JPEG;
-    
-    // Image quality settings
-    config.frame_size = FRAMESIZE_QVGA;
-    config.jpeg_quality = 12; // 0-63, lower means higher quality
-    config.fb_count = 1;
+  // Configure camera
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
 
-    // Initialize camera
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) {
-        Serial.printf("Camera init failed with error 0x%x", err);
-        return;
-    }
+  // Image quality settings
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 12;  // 0-63, lower means higher quality
+  config.fb_count = 1;
 
-    // Initialize BLE
-    BLEDevice::init("ESP32-CAM");
-    pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new ServerCallbacks());
+  // Initialize camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
 
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    pCharacteristic = pService->createCharacteristic(
-        CHARACTERISTIC_UUID,
-        BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_WRITE |
-        BLECharacteristic::PROPERTY_NOTIFY
-    );
-    pCharacteristic->addDescriptor(new BLE2902());
-    pService->start();
+  // Initialize BLE
+  BLEDevice::init("ESP32-CAM");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
 
-    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->setScanResponse(true);
-    pAdvertising->setMinPreferred(0x06);
-    pAdvertising->setMinPreferred(0x12);
-    BLEDevice::startAdvertising();
+  BLEService* pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+
+  BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
 }
 
 void loop() {
-    if (deviceConnected) {
-        // Capture frame
-        camera_fb_t * fb = esp_camera_fb_get();
-        if (!fb) {
-            Serial.println("Camera capture failed");
-            return;
-        }
-
-        // Send image size first
-        uint32_t imageSize = fb->len;
-        pCharacteristic->setValue((uint8_t*)&imageSize, sizeof(imageSize));
-        pCharacteristic->notify();
-        delay(20);  // Give some time for the notification to be sent
-
-        // Send image data in chunks
-        size_t chunks = (fb->len + MTU_SIZE - 1) / MTU_SIZE;
-        for (size_t i = 0; i < chunks; i++) {
-            size_t start = i * MTU_SIZE;
-            size_t end = min(fb->len, start + MTU_SIZE);
-            size_t chunkSize = end - start;
-            
-            pCharacteristic->setValue(fb->buf + start, chunkSize);
-            pCharacteristic->notify();
-            delay(20);  // Delay between chunks to prevent data loss
-        }
-
-        esp_camera_fb_return(fb);
-        delay(100);  // Delay between frames
+  if (deviceConnected) {
+    // Capture frame
+    camera_fb_t* fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return;
     }
-    delay(10);
+
+    // Send image size first
+    uint32_t imageSize = fb->len;
+    pCharacteristic->setValue((uint8_t*)&imageSize, sizeof(imageSize));
+    pCharacteristic->notify();
+    delay(20);  // Give some time for the notification to be sent
+
+    // Send image data in chunks
+    size_t chunks = (fb->len + MTU_SIZE - 1) / MTU_SIZE;
+    for (size_t i = 0; i < chunks; i++) {
+      size_t start = i * MTU_SIZE;
+      size_t end = min(fb->len, start + MTU_SIZE);
+      size_t chunkSize = end - start;
+
+      pCharacteristic->setValue(fb->buf + start, chunkSize);
+      pCharacteristic->notify();
+      delay(20);  // Delay between chunks to prevent data loss
+    }
+
+    esp_camera_fb_return(fb);
+    delay(100);  // Delay between frames
+  }
+  delay(10);
 }
